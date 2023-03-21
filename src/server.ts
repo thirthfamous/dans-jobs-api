dccac61b84c8ecb07db0e6f5de102eb8af474840
Farhan Hamdallah <fhbluebeast356@gmail.com>
2023-03-22 01:42:41 +0700

    add cors

--- src/server.ts
+++ src/server.ts
@@ -4,11 +4,13 @@ import loginRouter from "./routes/loginRoute";
 import "reflect-metadata";
 import { errorHandler } from "./middlewares/errorHandler";
 import jobRouter from "./routes/jobRoute";
+import cors from "cors";
 
 const app = express();
 
 dotenv.config();
 
+app.use(cors())
 app.use(express.json());
 
 app.use('/login', loginRouter)
@@ -16,8 +18,8 @@ app.use('/jobs', jobRouter)
 
 app.use(errorHandler)
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
+app.listen(8080, () => {
+  console.log('Server started on port 8080');
 });
 
 export default app
